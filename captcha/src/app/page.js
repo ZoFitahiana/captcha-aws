f0c6f9989febe08609f5aee3878f31a2a39e4864
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-12-19 08:25:42 +0100

    feat: fix bug

--- captcha/src/app/page.js
+++ captcha/src/app/page.js
@@ -4,20 +4,5 @@ import Form from './components/form';
 import Sequence from './components/sequence';
 
 export default function Home() {
-  const [number, setNumber] = useState(null);
-
-  const handleFormSubmit = (num) => {
-    setNumber(num);
-  };
-
-  return (
-    <div>
-      <h1>Forbidden Sequence Generator</h1>
-      {number === null ? (
-        <Form onSubmit={handleFormSubmit} />
-      ) : (
-        <Sequence number={number} />
-      )}
-    </div>
-  );
+  return "hello!";
 }
